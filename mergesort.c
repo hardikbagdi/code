695799de3698171861ae7b16d84d59f15e7a9796
Hardik Bagdi <hbagdi1@binghamton.edu>
2015-11-05 12:21:34 -0500

    removing files

--- mergesort.c
+++ mergesort.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void merge(int A[],int p, int q, int r){
+int merge(int A[],int p, int q, int r){
 	int i,j,k;
 	int n1,n2;
+	int inversions=0;
 	int n= r+1;
 	n1=q-p+1;
 	n2=r-q;
@@ -24,12 +25,15 @@ void merge(int A[],int p, int q, int r){
 	i=0;j=0;
 	k=p;
 	while(i<n1 && j<n2){
-
+	printf("\n \t inv:%d",inversions);
 		if(L[i]<=R[j]){
 			A[k]=L[i];
 			i++;
 		}
 		else{
+			if(i<n1)
+			{printf("\nn1=%d,i=%d",n1,i);inversions=inversions+n1-i;
+			printf("\ninv:%d",inversions);}
 			A[k]=R[j];
 			j++;
 		}
@@ -47,23 +51,31 @@ void merge(int A[],int p, int q, int r){
 				k++;
 		}
 
-
+return inversions;
 
 
 }
 
-void mergesort(int A[],int p,int r)
+int mergesort(int A[],int p,int r)
 {
+int inversions=0;
+int inversionsB=0;
+int inversionC=0;
 	int q,i;
 	int n= r-p+1;
 	printf("\n inside merge sort. array is\n");
 for(i=0;i<n;i++)
 	printf("%d\t",A[i] );
-if(p<r){
+if(p!=(r-1)){
 q= (p+r)/2;
-mergesort(A,p,q);
-mergesort(A,q+1,r);
-merge(A,p,q,r);
+inversionsB=mergesort(A,p,q);
+inversionC=mergesort(A,q+1,r);
+inversions=merge(A,p,q,r);
+printf("\n\n Inversions before add: %d", inversions);
+inversions=inversions+inversionC+inversionsB;
+printf("\n\n Inversions: %d, InvB: %d, InvC: %d",inversions,inversionsB,inversionC);
+
+return inversions;
 }
 
 }
@@ -71,6 +83,7 @@ void main(){
 
 int n,i;
 int *a;
+int inversions=0;
 printf("\n \n Enter size:");
 scanf("%d",&n);
 printf("size of pointer a is %d \n", sizeof(*a));
@@ -89,9 +102,13 @@ printf("\n \nArray before sort \n");
 for(i=0;i<n;i++)
 	printf("%d\t",a[i] );
 
-mergesort(a,0,n-1);
+inversions=mergesort(a,0,n-1);
 
 printf("\n \nArray after sort");
 for(i=0;i<n;i++)
 	printf("%d\t",a[i] );
+
+
+printf("\n\n Inversions: %d",inversions);
+
 }
